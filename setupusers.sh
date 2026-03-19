be9f9bc4bd53e9ad61988f956ec0305e21b21831
Taz <tazscotttalib@gmail.com>
2026-03-18 22:01:03 -0400

    add setup bash script

---
+++ setupusers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Usage: sudo setupusers.sh <names.txt>
+
+set -e
+
+F=$1
+
+if [ -z "$F" ]; then
+  echo "Usage: sudo setupusers.sh <names.txt>"
+  exit 1
+fi
+
+NAMES=$(cat $F)
+
+for name in ${NAMES[@]}
+do
+  echo "adding $name ..."
+  set +e
+  if id "$name" >/dev/null 2>&1; then
+    echo "$name already exists. Skipping"
+    continue
+  fi
+  set -e
+  useradd -d "/home/$name" -s /bin/bash -U -m --skel /home/zeus "$name"  
+  echo "added $name"
+done
+
+echo "Done creating all users"
